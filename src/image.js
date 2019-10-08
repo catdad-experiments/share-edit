01796d2615ed774551258ff299ada01e0aa449e4
Kiril Vatev <vatev.1@gmail.com>
2019-10-08 18:33:45 -0400

    image module renders images onto canvas

--- src/image.js
+++ src/image.js
@@ -7,16 +7,39 @@
     const context = this;
     const { events } = context;
 
-    events.on('display-image', ({ file }) => {
+    const canvas = document.querySelector('#canvas');
+    const container = canvas.parentElement;
+    const ctx = canvas.getContext('2d');
+
+    const onResize = () => {
+      const bb = container.getBoundingClientRect();
+      console.log(bb);
+
+      canvas.style.height = `${bb.height}px`;
+      canvas.style.width = `${bb.width}px`;
+      canvas.width = bb.width;
+      canvas.height = bb.height;
+    };
+
+    onResize();
+
+    const onFile = ({ file }) => {
       const img = new Image();
       const url = URL.createObjectURL(file);
       img.onload = () => {
         URL.revokeObjectURL(url);
+
+        ctx.drawImage(img, 0, 0);
       };
       img.src = url;
-      document.body.append(img);
-    });
+    };
 
-    return function destroy() {};
+    events.on('display-image', onFile);
+    window.addEventListener('resize', onResize);
+
+    return function destroy() {
+      events.off('display-image', onFile);
+      window.removeEventListener('resize', onResize);
+    };
   });
 })(window.registerModule);
