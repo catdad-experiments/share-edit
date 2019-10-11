8de1791fa362ac339a7a2a70d6f0428a0469fcfd
Kiril Vatev <vatev.1@gmail.com>
2019-10-10 21:29:47 -0400

    hoking up crop button to open crop palette

--- src/open.js
+++ src/open.js
@@ -1,20 +1,43 @@
+const find = selector => document.querySelector(selector);
+const findAll = selector => [...document.querySelectorAll(selector)];
+
 export default ({ events }) => {
-  const open = document.querySelector('#open');
-  const openInput = document.querySelector('#open-input');
+  const palettes = new Map([
+    ['general', find('.general-tools')],
+    ['crop', find('.crop-tools')]
+  ]);
+
+  const showPalette = name => void palettes.forEach((value, key) => {
+    if (key === name) {
+      value.classList.remove('hide');
+    } else {
+      value.classList.add('hide');
+    }
+  });
+
+  const open = find('#open');
+  const openInput = find('#open-input');
+  const crop = find('#crop');
+  const doneButtons = findAll('.controls .done');
 
   const onOpen = (ev) => {
     events.emit('display-image', { file: ev.target.files[0] });
   };
 
-  const onClick = () => {
-    openInput.click();
-  };
+  const onClick = () => void openInput.click();
+  const onDone = () => void showPalette('general');
+  const onCrop = () => void showPalette('crop');
 
   open.addEventListener('click', onClick);
   openInput.addEventListener('change', onOpen);
+  crop.addEventListener('click', onCrop);
+  doneButtons.forEach(done => done.addEventListener('click', onDone));
 
   return function destroy() {
     open.removeEventListener('click', onClick);
     openInput.removeEventListener('change', onOpen);
+    crop.removeEventListener('click', onCrop);
+
+    doneButtons.forEach(done => done.removeEventListener('click', onDone));
   };
 };
