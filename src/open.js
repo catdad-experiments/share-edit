29e43876ef15ea03336c2dc2d34078810802b867
Kiril Vatev <vatev.1@gmail.com>
2019-10-10 20:30:14 -0400

    using icons for the buttons

--- src/open.js
+++ src/open.js
@@ -1,13 +1,20 @@
 export default ({ events }) => {
   const open = document.querySelector('#open');
+  const openInput = document.querySelector('#open-input');
 
   const onOpen = (ev) => {
     events.emit('display-image', { file: ev.target.files[0] });
   };
 
-  open.addEventListener('change', onOpen);
+  const onClick = () => {
+    openInput.click();
+  };
+
+  open.addEventListener('click', onClick);
+  openInput.addEventListener('change', onOpen);
 
   return function destroy() {
-    open.removeEventListener('change', onOpen);
+    open.removeEventListener('click', onClick);
+    openInput.removeEventListener('change', onOpen);
   };
 };
