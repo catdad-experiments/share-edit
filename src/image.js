338a82e4a758b23c6d17d77622d7376b569d5b45
Kiril Vatev <vatev.1@gmail.com>
2019-10-10 22:18:07 -0400

    adding visible crop tool

--- src/image.js
+++ src/image.js
@@ -1,4 +1,48 @@
+const cropDiv = () => {
+  const div = document.createElement('div');
+  Object.assign(div.style, {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    outline: '1px solid pink'
+  });
+
+  [{
+    className: 'handle-n',
+    style: { top: '-4px', left: '25%', width: '50%', height: '8px' },
+    move: (bb) => {
+
+    }
+  }, {
+    className: 'handle-e',
+    style: { right: '-4px', top: '25%', width: '8px', height: '50%' },
+    move: () => {}
+  }, {
+    className: 'handle-s',
+    style: { bottom: '-4px', left: '25%', width: '50%', height: '8px' },
+    move: () => {}
+  }, {
+    className: 'handle-w',
+    style: { left: '-4px', top: '25%', width: '8px', height: '50%' },
+    move: () => {}
+  }].forEach(({ className, style, move }) => {
+    const handle = document.createElement('div');
+    Object.assign(handle.style, {
+      'background-color': 'pink',
+      position: 'absolute',
+    }, style);
+    handle.className = className;
+
+    div.appendChild(handle);
+  });
+
+  return div;
+};
+
 export default ({ events }) => {
+  const main = document.querySelector('.renderer');
   const canvas = document.querySelector('#canvas');
   const ctx = canvas.getContext('2d');
 
@@ -18,9 +62,24 @@ export default ({ events }) => {
     img.src = url;
   };
 
+  const onCrop = () => {
+    const div = cropDiv();
+    main.appendChild(div);
+
+    console.log('CROP ✂');
+  };
+
+  const onDone = () => {
+    console.log('DONE ✔');
+  };
+
   events.on('display-image', onFile);
+  events.on('controls-crop', onCrop);
+  events.on('controls-done', onDone);
 
   return function destroy() {
     events.off('display-image', onFile);
+    events.off('controls-crop', onCrop);
+    events.off('controls-done', onDone);
   };
 };
